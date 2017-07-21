c8b6e3bd5cb86c36cee39a55ce2565c2d159df0f
Jernej Zorec <jernejz@gmail.com>
2017-07-21 17:49:01 +0200

    Initial Commit

---
+++ FlickrWalkTests/FlickrWalkTests.swift
@@ -0,0 +1,36 @@
+//
+//  FlickrWalkTests.swift
+//  FlickrWalkTests
+//
+//  Created by Jernej Zorec on 21/07/2017.
+//  Copyright © 2017 Jernej Zorec. All rights reserved.
+//
+
+import XCTest
+@testable import FlickrWalk
+
+class FlickrWalkTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
